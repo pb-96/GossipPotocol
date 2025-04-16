6b2ae9a04a6102f05b3ce932ef17fa3e8f79a6c4
pb-96 <philip.byrne@faculty.ai>
2025-04-16 15:13:09 +0100

    updating run command

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./maelstrom test -w echo --bin ~/go/bin/maelstrom-echo --node-count 1 --time-limit 10
+cd maelstrom-echo/maelstrom && chmod +x maelstrom && ./maelstrom test -w echo --bin ~/go/bin/maelstrom-echo --node-count 1 --time-limit 10
