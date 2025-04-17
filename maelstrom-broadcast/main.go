618402265da9a9f1a503f599aaa8ce0bdd41e6f9
pb-96 <philip.byrne@faculty.ai>
2025-04-17 10:34:01 +0100

    adding seperate folders for each issue

---
+++ maelstrom-broadcast/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("consume", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		// Add the numbers from the broadcast to the given node itself
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		n.NodeIDs()
+		// Do broadcast here
+
+		body["type"] = "broadcast_ok"
+		// TODO: Implement broadcast
+		// How to attach nodes to broadcast to
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "topology_ok"
+		// TODO: Implement broadcast
+		// How to attach nodes to broadcast to
+
+		return n.Reply(msg, body)
+	})
+}
