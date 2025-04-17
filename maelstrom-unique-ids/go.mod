58a3ef287f55ccf5a40cb2452443c8d6a76d6847
pb-96 <philip.byrne@faculty.ai>
2025-04-17 12:58:44 +0100

    condesning into one folder

--- maelstrom-unique-ids/go.mod
+++
@@ -1,7 +0,0 @@
-module maelstrom-unique-ids
-
-go 1.23.6
-
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20250204203845-8263d1dd2b7a
-
-require github.com/satori/go.uuid v1.2.0 // indirect
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 cd maelstrom-echo/maelstrom && chmod +x maelstrom && ./maelstrom test -w echo --bin ~/go/bin/maelstrom-echo --node-count 1 --time-limit 10
-cd maelstrom-echo/maelstrom && chmod +x maelstrom && ./maelstrom test -w unique-ids --bin ~/go/bin/maelstrom-echo --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+cd maelstrom-unique-ids/maelstrom && chmod +x maelstrom && ./maelstrom test -w unique-ids --bin ~/go/bin/-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
