83dc9afa14502e8a7d57eeec969ba6e12d4ecd93
pb-96 <philip.byrne@faculty.ai>
2025-04-17 14:35:52 +0100

    adding support from single broadcast

--- maelstrom-ds/main.go
+++ maelstrom-ds/main.go
@@ -15,13 +15,15 @@ import (
 type ExtendedNode struct {
 	*maelstrom.Node                          // Embed the Maelstrom node
 	messages        map[interface{}]struct{} // Your local storage
+	seen_messages   map[interface{}]string   // Track which node sent us each message
 	// Add any other fields you need
 }
 
 func NewExtendedNode() *ExtendedNode {
 	return &ExtendedNode{
-		Node:     maelstrom.NewNode(),
-		messages: make(map[interface{}]struct{}, 0), // Initialize your storage
+		Node:          maelstrom.NewNode(),
+		messages:      make(map[interface{}]struct{}, 0), // Initialize your storage
+		seen_messages: make(map[interface{}]string, 0),   // Initialize seen messages tracking
 	}
 }
 
@@ -36,6 +38,25 @@ func gen_uuid() string {
 	timeStr := new(big.Int).SetBytes(timestamp).Text(62)
 
 	return timeStr + ":" + uuidStr
+}
+
+func flatten_topology(topology []string, given_node string) map[string][]string {
+
+	seen := make(map[string][]string)
+	other_nodes := make([]string, 0)
+	for _, node := range topology {
+		if node == given_node {
+			continue
+		} else {
+			other_nodes = append(other_nodes, node)
+		}
+	}
+	if len(other_nodes) == 0 {
+		return seen
+	} else {
+		seen[given_node] = other_nodes
+		return seen
+	}
 }
 
 func main() {
@@ -86,9 +107,19 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		// Add the numbers from the broadcast to the given node itself
-		n.messages[body["message"]] = struct{}{}
-		body["type"] = "consume_ok"
+
+		// Check if we've already processed this message
+		message := body["message"]
+		if _, exists := n.messages[message]; exists {
+			// We've already seen this message, no need to process it again
+			var merged_body map[string]any = map[string]any{
+				"type": "echo",
+			}
+			return n.Reply(msg, merged_body)
+		}
+
+		// Add the message to our storage
+		n.messages[message] = struct{}{}
 		return n.Reply(msg, body)
 	})
 
@@ -97,26 +128,44 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		all_channels := n.NodeIDs()
-		for _, channel := range all_channels {
-			message := map[string]any{
-				"type":    "consume",
-				"message": body["message"],
+
+		// Store the message locally
+		message := body["message"]
+		n.messages[message] = struct{}{}
+
+		// Get our topology
+		topology := flatten_topology(n.NodeIDs(), n.ID())
+
+		// Send to neighbors according to topology
+		if neighbors, ok := topology[n.ID()]; ok {
+			for _, neighbor := range neighbors {
+				forward_msg := map[string]any{
+					"type":    "consume",
+					"message": message,
+					"from":    n.ID(),
+				}
+				n.Send(neighbor, forward_msg)
 			}
-			n.Send(channel, message)
 		}
-		body["type"] = "broadcast_ok"
-		return n.Reply(msg, body)
+
+		return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
+		// var body map[string]any
+		// if err := json.Unmarshal(msg.Body, &body); err != nil {
+		// 	return err
+		// }
+		topology_as_map := flatten_topology(n.NodeIDs(), n.ID())
+		var merged_body map[string]any = map[string]any{
+			"type": "topology_ok",
 		}
-		body["topology"] = n.NodeIDs()
-		body["type"] = "topology_ok"
-		return n.Reply(msg, body)
+
+		if len(topology_as_map) > 0 {
+			merged_body["topology"] = topology_as_map
+		}
+
+		return n.Reply(msg, merged_body)
 	})
 
 	if err := n.Run(); err != nil {
