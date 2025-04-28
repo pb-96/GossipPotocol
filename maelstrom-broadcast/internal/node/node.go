12d09a2395d3d627e7f92ab810c0c7c75f35de19
pb-96 <philip.byrne@faculty.ai>
2025-04-28 15:38:14 +0100

    adding node

--- maelstrom-broadcast/internal/node/node.go
+++ maelstrom-broadcast/internal/node/node.go
@@ -1,1 +1,36 @@
 package node
+
+import (
+	"log"
+	"maelstrom-broadcast/internal/cache"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+// Node represents a node in the distributed system.
+type Node struct {
+	N        *maelstrom.Node // Reference to the Maelstrom node
+	Store    cache.KVStore   // Each node has got its own KV store
+	Topology []string        // Each node will have its own topology
+}
+
+func NewNode() *Node {
+	return &Node{
+		N:     maelstrom.NewNode(),
+		Store: *cache.NewCache(),
+	}
+}
+
+func (n *Node) SetTopology(config []string) {
+	n.Topology = config
+}
+
+func (n *Node) GetTopology() []string {
+
+	if len(n.Topology) == 0 {
+		log.Printf("no topology initialized for the node %s", n.N.ID())
+		return n.N.NodeIDs()
+	}
+
+	return n.Topology
+}
